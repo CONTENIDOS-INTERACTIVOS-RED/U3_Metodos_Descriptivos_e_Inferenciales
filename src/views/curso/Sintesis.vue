6cfb3d45e9739d1420afc669c6472eb4d590dcf5
Francisco Lizcano <flizcanor@icloud.com>
2025-08-10 22:42:09 -0500

    se actualiza sintesis

--- src/views/curso/Sintesis.vue
+++ src/views/curso/Sintesis.vue
@@ -2,9 +2,7 @@
   .curso-main-container.creditos-vista
     BannerInterno(subTitulo="SÍNTESIS")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-      p(data-aos="fade-up") La Unidad 2: Métodos inferenciales y pruebas de hipótesis, desarrolla las bases conceptuales y metodológicas necesarias para interpretar datos y respaldar decisiones estratégicas en contextos empresariales, mediante el uso de la estadística inferencial. Esta unidad profundiza en técnicas como la estimación puntual, los intervalos de confianza y las pruebas de hipótesis (paramétricas y no paramétricas), así como en el análisis de correlación y regresión, tanto simple como múltiple, para estudiar relaciones entre variables y predecir comportamientos futuros.
-
-      p.mb-5 Además, se explora el uso del valor p, la identificación de errores tipo I y II, y la aplicación de pruebas de significancia en modelos multivariados, fortaleciendo la capacidad analítica y crítica de los estudiantes. A lo largo del curso, se hace énfasis en el uso de herramientas tecnológicas como Excel, R, SPSS o Python, con el fin de aplicar los conocimientos a casos empresariales reales.
+      p(data-aos="fade-up") La Unidad 3: Aplicaciones en Inteligencia de Negocios - Análisis de datos en entornos empresariales, profundiza en el uso de herramientas estadísticas aplicadas al análisis de datos en contextos empresariales, enfocándose en la transformación de datos en conocimiento estratégico. Esta unidad permite al estudiante familiarizarse con los modelos estadísticos descriptivos, predictivos y prescriptivos, y su implementación en entornos de Inteligencia de Negocios mediante lenguajes de programación como R y Python, así como herramientas como SPSS y Power BI. A través de casos prácticos y simulaciones empresariales, se desarrolla la capacidad para interpretar resultados, validar modelos, automatizar procesos y tomar decisiones basadas en evidencia cuantitativa. Este enfoque aplicado, fortalece las competencias analíticas, tecnológicas y estratégicas necesarias para responder a los retos de un entorno empresarial orientado por datos.
 
       .row.justify-content-center
         .col-lg-12.mb-5
